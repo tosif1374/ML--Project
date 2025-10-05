67a21dfc394e0c0bc71c26f62c98db75d8b6eeac
mohammad tosif <mohammadtosif2004@gmail.com>
2025-10-06 01:10:22 +0530

    eda performance and training

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,25 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message(error, error_detail: sys):
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{}] line number [{}] error message [{}]".format()(
+    error_message = "Error occurred in python script name [{}] line number [{}] error message [{}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
+
 class CustomException(Exception):
-    def __init__(self,error, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message=error_message(error_message, error_detail=error_detail)
+    def __init__(self, error, error_detail: sys):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail)
 
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
 
+if __name__ == "__main__":
     try:
-        a=1/0
+        a = 1 / 0
     except Exception as e:
         logging.info("Divide by zero")
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
